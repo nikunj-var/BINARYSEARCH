454942d5630ba5be02a97a23c7fc17500bcd7b9a
Nikunj <nikunjvar2002@gmail.com>
2022-07-03 15:10:10 -0700

    searchinrotatedarray

---
+++ pivotindex.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int pivot(int arr[],int n){
+    int s=0,e=n-1;
+    int mid=s+(e-s)/2;
+    
+    while(s<e){
+       if(arr[mid]>=arr[0]){
+        s=mid+1;
+       }
+       else{
+        e=mid;
+       }
+       mid=s+(e-s)/2;
+    }
+    return s;
+}
+
+int main(){
+    int n,key;
+    cin>>n;
+    int arr[n];
+    cout<<"enter array";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"enter key";
+    cin>>key;
+    cout<<"pivot index"<<pivot(arr,n)<<endl;
+   
+}
